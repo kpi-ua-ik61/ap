84a1dbb2bc7588b53e04ed2c2e61c4b8b6cde863
Yevgeny Gnatyuk <gnatyuk.yevg@gmail.com>
2016-09-29 22:40:55 +0300

    ready

--- lab3.3/main.c
+++ lab3.3/main.c
@@ -20,21 +20,21 @@ int code() {
 	b = returnInputFloat("Enter b: ");
 	c = returnInputFloat("Enter c: ");
 
-	p = b - pow(a, 2) / 3;
-	printf("p: %f\n", p);
+	p = b - powf(a, 2) / 3;
+	// printf("p: %f\n", p);
 
-	q = (2 * pow(a, 3) / 27) - (a * b / 3) + c;
-	printf("q: %f\n\n", q);
+	q = (2 * powf(a, 3) / 27) - (a * b / 3) + c;
+	// printf("q: %f\n\n", q);
 
-	d = (pow(p, 3) / 27) + (pow(q, 2) / 4);
-	printf("d: %f\n\n", d);
+	d = (powf(p, 3) / 27) + (powf(q, 2) / 4);
+	// printf("d: %f\n\n", d);
 
 	if (d > 0){
 		float u = cbrtf(-q / 2 + sqrtf(d));
-		printf("u: %f\n", u);
+		// printf("u: %f\n", u);
 
 		float v = cbrtf(-q / 2 - sqrtf(d));
-		printf("v: %f\n\n", v);
+		// printf("v: %f\n\n", v);
 
 		float yone = u + v;
 		float xone = yone - a / 3;
@@ -47,31 +47,29 @@ int code() {
 	}
 	else if (d == 0){
 		float xone = 3 * q / p - a/3;
-		float xtwo = -1 * 3 * p / 2 / p - a/3;
+		float xtwo = -1 * 3 * q / 2 / p - a/3;
 		float xthree = xtwo;
 
-		printf("x1: %f\n", xone);
+		printf("\nx1: %f\n", xone);
 		printf("x2: %f\n", xtwo);
 		printf("x3: %f\n", xthree);
 
-		float check = pow(xone, 3) + a * pow(xtwo, 2) + b * xthree + c;
-		printf("\nCheck: %f", check);
+		// printf("\nCheck: %f", xone*xone*xone + a * xtwo * xtwo + b * xthree + c);
 		
 	}
 	else if (d < 0) {
-		float r = sqrtf(-1 * pow(p, 3) / 27);
-		float fi = acos(-1 * q / 2 * r);
+		float r = sqrtf(-1 * powf(p, 3) / 27);
+		float fi = acos(-q / 2 / r);
 
-		float xone = 2 * abs(cbrtf(r)) * cos(fi / 3) - a/3;
-		float xtwo = 2 * abs(cbrtf(r)) * cos((fi + 2 * M_PI) / 3) - a/3;
-		float xthree = 2 * abs(cbrtf(r)) * cos((fi + 4 * M_PI) / 3) - a/3;
+		float xone = 2 * fabs(cbrtf(r)) * cos(fi / 3) - a/3;
+		float xtwo = 2 * fabs(cbrtf(r)) * cos((fi + 2 * M_PI) / 3) - a/3;
+		float xthree = 2 * fabs(cbrtf(r)) * cos((fi + 4 * M_PI) / 3) - a/3;
 
-		printf("x1: %f\n", xone);
-		printf("x2: %f\n", xtwo);
-		printf("x3: %f\n", xthree);
+		printf("x1: %g\n", xone);
+		printf("x2: %g\n", xtwo);
+		printf("x3: %g\n", xthree);
 
-		float check = pow(xone, 3) + a * pow(xtwo, 2) + b * xthree + c;
-		printf("\nCheck: %f", check);
+		// printf("\nCheck: %f", xone*xone*xone + a * xtwo * xtwo + b * xthree + c);
 
 	}
 
@@ -95,5 +93,3 @@ int main() {
 
 	return 0;
 }
-
-
