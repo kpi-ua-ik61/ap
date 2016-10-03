b2eb551b2f1276ec9510f61c724626f0c05a6686
Andrey Yudov <yudovv@gmail.com>
2016-10-03 21:05:13 +0300

    Add files via upload

---
+++ 3_1_laba/3_1.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+
+/*float delta;
+float x; //chislo
+int y, h, icheck; //otvet
+float E;
+int eps; //tochnost'
+int k, i=0; //stepen'
+char term;*/
+ int i=0;
+    int k;
+    int eps;
+    float e=1;
+
+int main()
+{  
+    printf("Enter eps:\n");
+    eps=k;
+	scanf("%d", k);	
+	{
+	while( i<eps )
+	{
+		i=i+1;		
+		printf("FGF:%d\n", e);	
+		e=e*0.1;
+		getchar();	
+	} 
+	}
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*int main ()
+{
+	printf("Enter power:\n");
+	scanf("%d", k);
+		if( (k>0)&&(k%2==0) )
+			{
+				printf("Enter number to solve:\n");
+				scanf("%f", x);
+					if ( x>=0 )	
+						do
+						{						
+							y=1;
+							k-1=h;
+							delta=1	;
+							delta=((x/pow(y,h))-y)/k;
+							y += d		
+						}
+						while (fabs(delt)>=eps)
+			}
+		if( (k>0)&&(k%2!==0) )
+			{
+				printf("Enter number to solve:\n");
+				scanf("%f", x);
+			}
+		if ( (k<0)&&(k%2==0) )
+			{
+				printf("Enter number to solve:\n");
+				scanf("%f", x);
+					if ( x>0 )
+			}
+		if ( (k<0)&&(k%2!==0) )
+			{
+				printf("Enter number to solve:\n");
+				scanf("%f", x);
+				if ( x!==0 )
+				
+			}
+		else 
+			printf("x=0");
+	
+	
+	
+	
+	
+	
+	
+		return 0;
+}*/
