646b14ab8006c45ef19920570559fd78bdedfd96
Andrey Yudov <yudovv@gmail.com>
2016-10-12 05:49:42 +0300

    Add files via upload

--- 3_3_laba/3_3_laba.cpp
+++ 3_3_laba/3_3_laba.cpp
@@ -4,15 +4,13 @@
 #include <stdlib.h>
 
 char symbol, exitchar;
-float x1, x2, x3, z1, z2, z3, a, b, c, p, q, d, y2, y22, y3, y33;/*, u, v, ut, j, phi;*/
-/*float yo, yt, yth, ytt, ythth, xo, xt, xth;*/
-int j;	//for exit
+float x1, x2, x3, z1, z2, z3, a, b, c, p, q, d, y2, y22, y3, y33;
+int j;
 
 	enter_number_a()
 {
 do
 {	symbol=0;
-
 	printf ("Enter 'a'\n\n");
 	scanf ("%f%c", &a, &symbol);
 	if(symbol!='\n')
@@ -26,12 +24,10 @@ do
 while (j==0);
 }
 
-
 	enter_number_b()
 {
 do
 {	symbol=0;
-
 	printf ("\nEnter 'b'\n\n");
 	scanf ("%f%c", &b, &symbol);
 	if(symbol!='\n')
@@ -49,7 +45,6 @@ while (j==0);
 {
 do
 {	symbol=0;
-
 	printf ("\nEnter 'c'\n\n");
 	scanf ("%f%c", &c, &symbol);
 	if(symbol!='\n')
@@ -61,24 +56,22 @@ do
 	else (j=1);					
 }
 while (j==0);
-
-
-
 }
+
 	calculating_p()	//OK
 {
 	p=(3*b-a*a)/3;	
 }
+
 	calculating_q()	//OK
 {
 	q=(2*a*a*a-9*a*b+27*c)/27;
 }
+
 	calculating_d()	//OK
 {
 	d=(p*p*p)/27 + (q*q)/4;
 }
-
-
 
 	if_d_more_0()
 {	
@@ -92,7 +85,6 @@ while (j==0);
 		z2=-(u+v)/2;
 		y3=y2;
 		z3=z2;
-
 		x1=z1-(a/3);
 		x2=z2-(a/3);
 		x3=z3-(a/3);
@@ -106,8 +98,7 @@ while (j==0);
 	{	
 		z1=3*q/p;
 		z2=-(3*q)/(2*p);
-		z3=z2;
-		
+		z3=z2;		
 		x1=z1-(a/3);
 		x2=z2-(a/3);
 		x3=z3-(a/3);
@@ -123,8 +114,7 @@ while (j==0);
 		f=acos(-q/(2*r));
 		z1=2*fabs(cbrtf(r))*cos(f/3);
 		z2=2*fabs(cbrtf(r))*cos((f+2*M_PI)/3);
-		z3=2*fabs(cbrtf(r))*cos((f+4*M_PI)/3);
-		
+		z3=2*fabs(cbrtf(r))*cos((f+4*M_PI)/3);		
 		x1=z1-(a/3);
 		x2=z2-(a/3);
 		x3=z3-(a/3);
@@ -143,8 +133,7 @@ while (j==0);
 	} 
 
 }
-	
-	
+		
 	chek_a_b_c_1()
 {
 	if ( (b==((a*a)/3)) && (c==((a*a*a)/27)) )
@@ -156,59 +145,38 @@ while (j==0);
 		y3=0;
 	} 
 }
+
 	calculation()
 {		
-	enter_number_a();
-	
-	enter_number_b();
-		
-	enter_number_c();				
-		
-	printf ("\nInput: x^3+%f*x^2+%f*x+%f=0\n\n", a, b, c);
-	
-	calculating_p();
-		
-	calculating_q();
-		
-	calculating_d();
-		
-	if_d_more_0();
-		
-	if_d_ecual_0();
-		
-	if_d_under_0();
-		
-	chek_a_b_c_0();	
-	
-	chek_a_b_c_1();
-	
-	printf ("x1=%.4f\n\n", x1);
-			
-	printf ("x2=%.4f-i*(%.4f)\n\n", x2,y2);
-		
+	enter_number_a();	
+	enter_number_b();		
+	enter_number_c();						
+	printf ("\nInput: x^3+%f*x^2+%f*x+%f=0\n\n", a, b, c);	
+	calculating_p();		
+	calculating_q();		
+	calculating_d();		
+	if_d_more_0();		
+	if_d_ecual_0();		
+	if_d_under_0();		
+	chek_a_b_c_0();		
+	chek_a_b_c_1();	
+	printf ("x1=%.4f\n\n", x1);			
+	printf ("x2=%.4f-i*(%.4f)\n\n", x2,y2);		
 	printf ("x3=%.4f+i*(%.4f)\n\n", x3,y3);
 }
 
 int main ()
 {
-	
 	do 
 	{
-		system("cls");
-		
-		printf ("\nSolve x^3+a*x^2+b*x+c=0\n\n");
-		
-		printf ("Results will be rounded to 4 decimal places.\n\n");
-		
-		calculation();
-		
-		printf ("Enter y to continue\n");
-		
-		exitchar = getchar();
-		
+		system("cls");		
+		printf ("\nSolve x^3+a*x^2+b*x+c=0\n\n");		
+		printf ("Results will be rounded to 4 decimal places.\n\n");		
+		calculation();		
+		printf ("Enter y to continue\n");		
+		exitchar = getchar();		
 	}
-	while ( (exitchar == 'y')||(exitchar == 'Y') );
-	 
+	while ( (exitchar == 'y')||(exitchar == 'Y') );	 
 	return 0;
 }
 
