ff4ee0bd639b792b4172aaa6fe81cc839a2e5b93
Yevgeny Gnatyuk <gnatyuk.yevg@gmail.com>
2016-10-12 11:24:18 +0300

    lib added pseudo-random generators

--- lib/inputlib/inputlib.h
+++ lib/inputlib/inputlib.h
@@ -2,3 +2,7 @@ int inputInt(int *value, char* request);
 int returnInputInt(char* request);
 int inputFloat(float *value, char* request);
 float returnInputFloat(char* request);
+int returnEps();
+float returnPrecision(int eps);
+int randInt(int min, int max);
+float randFloat();
