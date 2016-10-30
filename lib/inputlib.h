001aa287dc7375ac954f0e69c6f643fb1043ed48
Yevgeny Gnatyuk <gnatyuk.yevg@gmail.com>
2016-10-30 02:01:14 +0200

    library structure changed, rand lib added

--- lib/inputlib.h
+++ lib/inputlib.h
@@ -117,21 +117,3 @@ float returnPrecision(int eps){
 
 	return precision;
 }
-
-int randInt(int min, int max){
-	int num = min + (rand() % max+1-min);
-	int sign = rand() % 2;
-	if(min < 0 && sign) num *= -1;
-	return num;
-}
-
-float randFloat(/*float min, float max*/){
-	//float num = ((float)rand()/(float)(RAND_MAX)) * max + min;
-
-	float num = ((float)rand()/(float)(RAND_MAX)) * 100;
-
-	int sign = rand() % 2;
-	if(sign) num *= -1;
-
-	return num;
-}
---
+++ lib/rand.h
@@ -0,0 +1,21 @@
+#include <time.h>
+
+srand(time(0));
+
+int randInt(int min, int max){
+	int num = min + (rand() % max+1-min);
+	int sign = rand() % 2;
+	if(min < 0 && sign) num *= -1;
+	return num;
+}
+
+float randFloat(/*float min, float max*/){
+	//float num = ((float)rand()/(float)(RAND_MAX)) * max + min;
+
+	float num = ((float)rand()/(float)(RAND_MAX)) * 100;
+
+	int sign = rand() % 2;
+	if(sign) num *= -1;
+
+	return num;
+}
