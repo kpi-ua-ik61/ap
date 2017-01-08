634fd11b694f15d72f0debe08582ee9f45a53cc9
Yevgeny Gnatyuk <gnatyuk.yevg@gmail.com>
2017-01-08 18:58:22 +0200

    lab9 update: signature, random, ...

--- lab9/main.c
+++ lab9/main.c
@@ -5,7 +5,7 @@
 #include <string.h>
 
 #include "../lib/inputlib.h"
-// #include "../lib/rand.h"
+#include "../lib/rand.h"
 
 #define BLACK 	"\033[0;30m"
 #define RED 	"\033[0;31m"
@@ -66,17 +66,19 @@ int file_open = 0;
 FILE *fp;
 char filename[127] = " ";
 
+char sign[28] = "egnatyuk-ap-lab9-Dp9vJXvorT\0";
+
 struct record{
-	char data[100]; 
-	double a;
-	double b;
+	char city[58]; 
+	double area;
+	int population;
 };
 
 
 
 int check_empty() {
 	fseek(fp, 0L, SEEK_END);
-	if(ftell(fp) == 0) return 1;
+	if(ftell(fp) == sizeof(sign)) return 1;
 	else return 0;
 }
 
@@ -89,17 +91,20 @@ int read_file() {
 
 	fseek(fp, 0L, SEEK_END);
 	fgetpos(fp, &end_pos);
-	rewind(fp);
+	fseek(fp, sizeof(sign), SEEK_SET);
 	fgetpos(fp, &cur_pos);
-	ent_count = end_pos / sizeof(struct record);
+	ent_count = (end_pos-sizeof(sign)) / sizeof(struct record);
 
 	printf("File %s contains %lld record(s)\n", filename, ent_count);
+
+	printf("| %4s | %58s | %12s | %12s |\n", "#", "City", "Area", "Population");
 
 	while (cur_pos < end_pos/* + sizeof(struct record)*/)
 	{
 		fread(rec, sizeof(struct record), 1, fp);
 		fgetpos(fp, &cur_pos);
-		printf("| %d	| %s 	| %f 	| %f 	|\n", i, rec->data, rec->a, rec->b);
+		printf("| %4d | %58s | %12g | %12d |\n", i, rec->city, rec->area, rec->population);
+		// if(i % 5 == 0) getch();
 		i++;
 	}
 
@@ -132,7 +137,79 @@ int create_file() {
 		return 0;
 	}
 
+	// add signature
+	fseek(fp, 0L, SEEK_SET);
+	fwrite(sign, sizeof(sign), 1, fp);
+
 	printf("Empty file "LGREEN"%s"NC" created!\n", filename);
+
+	if(returnBoolInt("Open this file? [y/n]: ") == 1){
+		file_open = 1;
+		read_file();
+	}
+	else {
+		if (fclose(fp)) {
+			printf("\n" LRED"Error"NC " Error while closing file!\n");
+			getch();
+			return 0;
+		}
+	}
+
+	return 0;
+}
+
+int create_file_random() {
+
+	printf("Enter file name: ");
+	scanf("%s", filename);
+	fflush(stdin);
+	
+	if ((fp=fopen(filename, "rb")) != NULL) {
+		printf(LRED"Error"NC " Maybe such file already exists!\n");
+		getch();
+		return 0;
+	}
+
+	if ((fp=fopen(filename, "wb")) == NULL) {
+		printf(LRED"Error"NC " Unable to create file! Maybe such file already exists!\n");
+		getch();     // exit(0);
+		return 0;
+	}
+
+	if ((fp=fopen(filename, "r+wb")) == NULL) {
+		printf(LRED"Error"NC " Unable to create file! Maybe such file already exists!\n");
+		getch();
+		return 0;
+	}
+
+	// add signature
+	fseek(fp, 0L, SEEK_SET);
+	fwrite(sign, sizeof(sign), 1, fp);
+
+	printf("File "LGREEN"%s"NC" created!\n", filename);
+
+	int num = returnMinInputInt("Enter number of records: ", 0);
+
+	struct record *tmp = malloc(sizeof(struct record));
+	
+	for(int i = 0; i < num; i++) {
+		fseek(fp, 0L, SEEK_END);
+
+		char new[58];
+		int string_length = randInt(3, 58);
+		for(int i = 0; i < string_length-1; i++){
+			new[i] = (char) randInt(97, 122);
+		}
+		new[string_length] = '\0';
+
+		strcpy(tmp->city, new);
+		tmp->area = randPosFloat();
+		tmp->population = randInt(0, 2147483647);
+
+		fwrite(tmp, sizeof(struct record), 1, fp);
+	}
+
+	printf(LGREEN"%d "NC"records created!\n", num);
 
 	if(returnBoolInt("Open this file? [y/n]: ") == 1){
 		file_open = 1;
@@ -158,6 +235,17 @@ int open_file() {
 	if ((fp=fopen(filename, "r+wb")) == NULL) {
 		printf(LRED"Error"NC " Unable to open file! Maybe file does not exist!\n");
 		getch();     // exit(0);
+		return 0;
+	}
+
+	char sign_check[28] = "";
+
+	fseek(fp, 0L, SEEK_SET);
+	fread(sign_check, sizeof(sign), 1, fp);
+
+	if(strcmp(sign_check, sign) != 0){
+		printf(LRED"Error"NC " This file can't be opened with this programm!\n");
+		getch();
 		return 0;
 	}
 
@@ -200,14 +288,15 @@ int close_file() {
 
 
 int create_record() {
-	char new[100];
+	char new[58];
 
 	fseek(fp, 0L, SEEK_END);
 
-	printf("Enter string: ");
-	
+	printf("Enter city name: ");
+
 	fflush(stdin);
-	fgets (new, 100, stdin);
+
+	fgets (new, 58, stdin);
 
 	if ((strlen(new)>0) && (new[strlen (new) - 1] == '\n'))
 		new[strlen (new) - 1] = '\0';
@@ -217,9 +306,9 @@ int create_record() {
 
 	struct record *tmp = malloc(sizeof(struct record));
 
-	strcpy(tmp->data, new);
-	tmp->a = returnInputDouble("Enter double 1: ");
-	tmp->b = returnInputDouble("Enter double 2: ");
+	strcpy(tmp->city, new);
+	tmp->area = returnInputDouble("Enter area: ");
+	tmp->population = returnMinInputInt("Enter population: ", 0);
 
 	fwrite(tmp, sizeof(struct record), 1, fp);
 
@@ -233,26 +322,27 @@ int read_record() {
 	long long end_pos;
 	long long ent_count;
 	int num = 0;
-	struct record *ret = malloc(sizeof(struct record));
+	struct record *rec = malloc(sizeof(struct record));
 
 	fseek(fp, 0L, SEEK_END);
 	fgetpos(fp, &end_pos);
-	rewind(fp);
+	fseek(fp, sizeof(sign), SEEK_SET);
 	fgetpos(fp, &cur_pos);
-	ent_count = end_pos / sizeof(struct record);
+	ent_count = (end_pos-sizeof(sign)) / sizeof(struct record);
 	printf("File %s contains %lld records\n", filename, ent_count);
 
 	num = returnMinMaxInputInt("Enter record number to show: ", 1, ent_count);
-	fseek(fp, sizeof(struct record) * (num-1), SEEK_SET);
-	fread(ret, sizeof(struct record), 1, fp);
-	printf("| %s 	| %f 	| %f 	|\n", ret->data, ret->a, ret->b);
+	fseek(fp, sizeof(sign) + sizeof(struct record) * (num-1), SEEK_SET);
+	fread(rec, sizeof(struct record), 1, fp);
+	printf(" %58s | %12s | %12s |\n", "City", "Area", "Population");
+	printf(" %58s | %12g | %12d |\n", rec->city, rec->area, rec->population);
 	
 	getch();
 	return 0;
 }
 
 int edit_record() {
-	char new[100];
+	char new[58];
 	long long cur_pos;
 	long long end_pos;
 	long long ent_count;
@@ -261,31 +351,32 @@ int edit_record() {
 
 	fseek(fp, 0L, SEEK_END);
 	fgetpos(fp, &end_pos);
-	rewind(fp);
+	fseek(fp, sizeof(sign), SEEK_SET);
 	fgetpos(fp, &cur_pos);
-	ent_count = end_pos / sizeof(struct record);
+	ent_count = (end_pos-sizeof(sign)) / sizeof(struct record);
 	printf("File %s contains %lld records\n", filename, ent_count);
 
 	num = returnMinMaxInputInt("Enter record number to edit: ", 1, ent_count);
 
-	fseek(fp, sizeof(struct record) * (num-1), SEEK_SET);
+	fseek(fp, sizeof(sign) + sizeof(struct record) * (num-1), SEEK_SET);
 	fread(tmp, sizeof(struct record), 1, fp);
-	printf("| %s 	| %f 	| %f 	|\n", tmp->data, tmp->a, tmp->b);
+	printf(" %58s | %12s | %12s |\n", "City", "Area", "Population");
+	printf(" %58s | %12g | %12d |\n", tmp->city, tmp->area, tmp->population);
 
-	printf("Enter new string: ");
+	printf("Enter new city name: ");
 
-	fgets (new, 100, stdin);
+	fgets (new, 58, stdin);
 
 	if ((strlen(new)>0) && (new[strlen (new) - 1] == '\n'))
 		new[strlen (new) - 1] = '\0';
 	
 	fflush(stdin);
 
-	strcpy(tmp->data, new);
-	tmp->a = returnInputDouble("Enter new double 1: ");
-	tmp->b = returnInputDouble("Enter new double 2: ");
+	strcpy(tmp->city, new);
+	tmp->area = returnInputDouble("Enter new area: ");
+	tmp->population = returnMinInputInt("Enter new population: ", 0);
 
-	fseek(fp, sizeof(struct record) * (num-1), SEEK_SET);
+	fseek(fp, sizeof(sign) + sizeof(struct record) * (num-1), SEEK_SET);
 	fwrite(tmp, sizeof(struct record), 1, fp);
 
 	printf(LGREEN"Record edited!\n"NC);
@@ -300,16 +391,16 @@ int sort_record() {
 
 	fseek(fp, 0L, SEEK_END);
 	fgetpos(fp, &end_pos);
-	rewind(fp);
+	fseek(fp, sizeof(sign), SEEK_SET);
 	fgetpos(fp, &cur_pos);
-	ent_count = end_pos / sizeof(struct record);
+	ent_count = (end_pos-sizeof(sign)) / sizeof(struct record);
 
 	if(ent_count < 2){
 		printf(LRED"Error"NC" You should have 2+ records to sort!\n");
 		getch();
 		return 0;
 	}
-	printf("Sort records of double1\n");
+	printf("Sort records by area\n");
 	if(returnADInt("Sort ascending/descending [a/d]: ") == 1){
 		// asc
 
@@ -318,21 +409,21 @@ int sort_record() {
 				struct record *olda = malloc(sizeof(struct record));
 				struct record *oldb = malloc(sizeof(struct record));
 
-				fseek(fp, sizeof(struct record) * a, SEEK_SET);
+				fseek(fp, sizeof(sign) + sizeof(struct record) * a, SEEK_SET);
 				fread(olda, sizeof(struct record), 1, fp);
 				
-				fseek(fp, sizeof(struct record) * b, SEEK_SET);
+				fseek(fp, sizeof(sign) + sizeof(struct record) * b, SEEK_SET);
 				fread(oldb, sizeof(struct record), 1, fp);
 			
-				double av = olda->a;
-				double bv = oldb->a;
+				double av = olda->area;
+				double bv = oldb->area;
 
 				if(av > bv){ 
 
-					fseek(fp, sizeof(struct record) * a, SEEK_SET);			// a = b
+					fseek(fp, sizeof(sign) + sizeof(struct record) * a, SEEK_SET);			// a = b
 					fwrite(oldb, sizeof(struct record), 1, fp);
 
-					fseek(fp, sizeof(struct record) * b, SEEK_SET);			// b = a
+					fseek(fp, sizeof(sign) + sizeof(struct record) * b, SEEK_SET);			// b = a
 					fwrite(olda, sizeof(struct record), 1, fp);
 				}
 			}
@@ -344,21 +435,21 @@ int sort_record() {
 				struct record *olda = malloc(sizeof(struct record));
 				struct record *oldb = malloc(sizeof(struct record));
 
-				fseek(fp, sizeof(struct record) * a, SEEK_SET);
+				fseek(fp, sizeof(sign) + sizeof(struct record) * a, SEEK_SET);
 				fread(olda, sizeof(struct record), 1, fp);
 				
-				fseek(fp, sizeof(struct record) * b, SEEK_SET);
+				fseek(fp, sizeof(sign) + sizeof(struct record) * b, SEEK_SET);
 				fread(oldb, sizeof(struct record), 1, fp);
 			
-				double av = olda->a;
-				double bv = oldb->a;
+				double av = olda->area;
+				double bv = oldb->area;
 
 				if(av < bv){ 
 
-					fseek(fp, sizeof(struct record) * a, SEEK_SET);			// a = b
+					fseek(fp, sizeof(sign) + sizeof(struct record) * a, SEEK_SET);			// a = b
 					fwrite(oldb, sizeof(struct record), 1, fp);
 
-					fseek(fp, sizeof(struct record) * b, SEEK_SET);			// b = a
+					fseek(fp, sizeof(sign) + sizeof(struct record) * b, SEEK_SET);			// b = a
 					fwrite(olda, sizeof(struct record), 1, fp);
 				}
 			}
@@ -371,7 +462,7 @@ int sort_record() {
 }
 
 int insert_record() {
-	char new[100];
+	char new[58];
 	long long cur_pos;
 	long long end_pos;
 	long long ent_count;
@@ -380,16 +471,16 @@ int insert_record() {
 
 	fseek(fp, 0L, SEEK_END);
 	fgetpos(fp, &end_pos);
-	rewind(fp);
+	fseek(fp, sizeof(sign), SEEK_SET);
 	fgetpos(fp, &cur_pos);
-	ent_count = end_pos / sizeof(struct record);
+	ent_count = (end_pos-sizeof(sign)) / sizeof(struct record);
 	printf("File %s contains %lld records\n", filename, ent_count);
 
 	num = returnMinMaxInputInt("Enter where to insert: ", 1, ent_count);
 
-	printf("Enter string: ");
+	printf("Enter city name: ");
 
-	fgets (new, 100, stdin);
+	fgets (new, 58, stdin);
 
 	if ((strlen(new)>0) && (new[strlen (new) - 1] == '\n'))
 		new[strlen (new) - 1] = '\0';
@@ -397,12 +488,12 @@ int insert_record() {
 	fflush(stdin);
 	
 	struct record *tmp = malloc(sizeof(struct record));
-	strcpy(tmp->data, new);
-	tmp->a = returnInputDouble("Enter double 1: ");
-	tmp->b = returnInputDouble("Enter double 2: ");
+	strcpy(tmp->city, new);
+	tmp->area = returnInputDouble("Enter area: ");
+	tmp->population = returnMinInputInt("Enter population: ", 0);
 
 	for(int i = 1; i <= ent_count-num+2; i++){
-		fseek(fp, sizeof(struct record) * -i, SEEK_END);  
+		fseek(fp, sizeof(struct record) * -i, SEEK_END);
 		fread(rec, sizeof(struct record), 1, fp);
 		
 		fseek(fp, sizeof(struct record) * 0, SEEK_CUR);
@@ -411,7 +502,7 @@ int insert_record() {
 		// read_file();
 	}
 
-	fseek(fp, sizeof(struct record) * (num - 1), SEEK_SET);
+	fseek(fp, sizeof(sign) + sizeof(struct record) * (num - 1), SEEK_SET);
 	fwrite(tmp, sizeof(struct record), 1, fp);
 
 	printf(LGREEN"Record inserted!\n"NC);
@@ -430,28 +521,36 @@ int delete_record() {
 
 	fseek(fp, 0L, SEEK_END);
 	fgetpos(fp, &end_pos);
-	rewind(fp);
+	fseek(fp, sizeof(sign), SEEK_SET);
 	fgetpos(fp, &cur_pos);
-	ent_count = end_pos / sizeof(struct record);
+	ent_count = (end_pos-sizeof(sign)) / sizeof(struct record);
 	printf("File %s contains %lld records\n", filename, ent_count);
 
 	num = returnMinMaxInputInt("Enter record number to delete: ", 1, ent_count);
 	
 	FILE *ptr2 = fopen("temp","a");
-	int foo = 0;
 
+	fseek(ptr2, 0L, SEEK_SET);
+	fwrite(sign, sizeof(sign), 1, ptr2);
 
+	fseek(fp, sizeof(sign), SEEK_SET);
+	fseek(ptr2, sizeof(sign), SEEK_SET);
+ 
 	for(int i = 1; i <= ent_count; i++){
-		fread(rec, sizeof(struct record), 1, fp);
+		if(i != num) {
+			fseek(fp, sizeof(sign) + sizeof(struct record) * (i-1), SEEK_SET);
+			fread(rec, sizeof(struct record), 1, fp);
 
-		if(i != num) fwrite(rec, sizeof(struct record), 1, ptr2);
-		// printf("%d\n", i);
+			fseek(ptr2, 0L, SEEK_END);
+			fwrite(rec, sizeof(struct record), 1, ptr2);
+		}
 	}
 
 	fclose(fp);
 	fclose(ptr2);
 	remove(filename);
 	rename("temp", filename);
+	remove("temp");
 	fp = fopen(filename, "r+b");
 
 	printf(LGREEN"Record deleted!\n"NC);
@@ -468,8 +567,9 @@ int file_menu(){
 	printf("------  File  ------\n\n");
 
 	printf("[1] Create new file\n");
-	printf("[2] Open file\n");
-	printf("[3] Delete file\n");
+	printf("[2] Create new file with random records\n");
+	printf("[3] Open file\n");
+	printf("[4] Delete file\n");
 	printf("\n");
 	printf("[0] Exit\n");
 
@@ -486,11 +586,15 @@ int file_menu(){
 						check = 0;
 						break;
 
-			case '2':	open_file();
+			case '2':	create_file_random();
 						check = 0;
 						break;
 
-			case '3':	remove_file();
+			case '3':	open_file();
+						check = 0;
+						break;
+
+			case '4':	remove_file();
 						check = 0;
 						break;
 
@@ -614,6 +718,7 @@ int edit_menu(){
 
 
 int main() {
+	randInit();
 
 	while(1){
 		if(file_open == 1){
